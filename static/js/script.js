6b75bef0ea20157be90ef9b508ba80c0b44e7d95
Swetha <swethaswetha6833@gmail.com>
2024-10-13 12:17:17 +0530

    deleted files

--- static/js/script.js
+++
@@ -1,9 +0,0 @@
-// static/js/script.js
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('Document is ready!');
-});
-
-function toggleMenu() {
-    const menu = document.getElementById('menu');
-    menu.classList.toggle('active');
-}
