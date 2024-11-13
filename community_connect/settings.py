0e3cae23d55a18117087d1933bc817b2c75a11e6
swethaedaguttu <140315099+swethaedaguttu@users.noreply.github.com>
2024-11-13 11:36:12 +0530

    Update settings.py

--- community_connect/settings.py
+++ community_connect/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'community_connect.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'events', 'templates','events')],
+        'DIRS': [os.path.join(BASE_DIR, 'events', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
