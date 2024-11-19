e43d06841ae5cb199e9ca9af614081f27bbb449f
Swetha <swethaswetha6833@gmail.com>
2024-11-19 12:43:46 +0530

    Normalize line endings

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\community_connect\community_connect\venv
+set VIRTUAL_ENV=C:\Communion-2\community_connect - Copy\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
